ef010464e008996c3e0859687f131ae5400666ec
CuongB19DCCN076 <91614370+CuongB19DCCN076@users.noreply.github.com>
2024-03-16 13:48:52 +0700

    Update JwtAuthenticationFilter.java

--- src/main/java/bookcarupdate/bookcar/config/JwtAuthenticationFilter.java
+++ src/main/java/bookcarupdate/bookcar/config/JwtAuthenticationFilter.java
@@ -31,7 +31,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             @NonNull FilterChain filterChain
     ) throws ServletException, IOException {
         final String authHeader = request.getHeader("Authorization");
-        System.out.println(authHeader);
+        System.out.println("auth:" + authHeader);
         final String jwt;
         final String userEmail;
 
